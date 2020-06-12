1f04ebb607aff6a856b042757c116f7f5ec6de90
theo <theo.simoes@hotmail.fr>
2020-06-12 15:34:42 +0200

    init ProfilPage

--- 3DOK/src/app/profil/profil.page.spec.ts
+++ 3DOK/src/app/profil/profil.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { Tab3Page } from './tab3.page';
+import { ProfilPage } from './profil.page';
 
 describe('Tab3Page', () => {
-  let component: Tab3Page;
-  let fixture: ComponentFixture<Tab3Page>;
+  let component: ProfilPage;
+  let fixture: ComponentFixture<ProfilPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab3Page],
+      declarations: [ProfilPage],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab3Page);
+    fixture = TestBed.createComponent(ProfilPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
--- 3DOK/src/app/tabs/tabs-routing.module.ts
+++ 3DOK/src/app/tabs/tabs-routing.module.ts
@@ -28,12 +28,12 @@ const routes: Routes = [
         ]
       },
       {
-        path: 'tab3',
+        path: 'profil',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('../profil/profil.module').then(m => m.ProfilPageModule)
           }
         ]
       },
